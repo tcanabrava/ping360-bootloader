2c50d990caefd06c0d661364dd846be222f3b546
Jacob Walser <jwalser90@gmail.com>
2019-08-28 18:37:55 -0400

    build-ping360-bootloader.sh: build both x86_64 and arm-linux-gnueabihf

--- tools/build-ping360-bootloader.sh
+++ tools/build-ping360-bootloader.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+export PATH=$(pwd)/arm8/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin:$PATH
+
 mkdir -p build
 cd build
 cmake ..
 make ping360-bootloader
+cmake -DRASPBERRY_PI="" ..
+make ping360-bootloader-rpi
 cd ..
